89691f9202840bcabc60a4c6d6211d2aaeb33d69
MadarasiD <98647320+MadarasiD@users.noreply.github.com>
2023-01-05 18:51:33 +0100

    php comparison and logical operators

---
+++ comparison_operators.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h2>Összehasonlító operátorok</h2>
+    <pre>
+    
+        egyenlő ==
+        azonos ===
+        Összehasonlító > < , >= , <>
+        nem egyenlő !=
+        nem azonos !==
+
+</pre>
+
+<h2>Logikai operátorok</h2>
+    <pre>
+    
+        és &&
+        vagy ||
+        nem !
+
+</pre>
+
+<?php 
+
+ if( 4 === '4' || 5 < 10 ) {
+
+    echo "ez igaz";
+
+ };
+    
+
+
+
+?>
+    
+</body>
+</html>
