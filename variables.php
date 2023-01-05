1462d2330d2b67bcc6a8085910404b91e9bd4607
MadarasiD <98647320+MadarasiD@users.noreply.github.com>
2023-01-05 17:05:23 +0100

    variable

---
+++ variables.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php 
+    
+    $name = "Madarasi";
+    $number = 100;
+    $Number_List = 100.5;
+    // $num-ber= 400;
+    // $0number = 500;
+    $NUMBER = 100;
+    $NUMBeR = 100.3;
+    
+
+    echo $name . " " . $number;
+
+    ?>
+    
+</body>
+</html>
