c226fdbb1a009316b5f8bd033d879f2dce0197a3
MadarasiD <98647320+MadarasiD@users.noreply.github.com>
2023-01-05 19:54:58 +0100

    php functions

---
+++ functions.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funcionts </title>
+</head>
+<body>
+
+<?php 
+
+function init () {
+
+    say_Something();
+    echo "<br>";
+    calculate();
+
+}
+
+function say_Something() {
+
+    echo "Hello, az én nevem Madarasi Dániel";
+
+    
+
+
+}
+
+
+
+
+
+function calculate() {
+
+    echo 456 + 345;
+
+
+}
+
+init();
+
+
+?>
+    
+</body>
+</html>
