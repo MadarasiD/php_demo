58b46b5c83cbc1181c008d66fc5a0fc6fc4944fb
MadarasiD <98647320+MadarasiD@users.noreply.github.com>
2023-01-05 17:36:05 +0100

    php array

---
+++ array.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+
+$numberList = array( 23, 64, 267, 555, 851, '5435', 345, '<h1>Hello</h1>');
+
+// $numberList = [2,25,555];
+
+print_r($numberList);
+
+// echo $numberList[0];
+
+
+
+?>
+    
+</body>
+</html>
