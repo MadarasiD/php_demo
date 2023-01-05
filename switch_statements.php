417ec5c9e690223e64485a98e28e92a5156238cb
MadarasiD <98647320+MadarasiD@users.noreply.github.com>
2023-01-05 19:01:40 +0100

    php switch statement

---
+++ switch_statements.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+
+$number = 10;
+
+switch( $number ) {
+
+    case 34:
+    echo "ez itt 34";
+    break;
+    case 37:
+    echo "ez itt 37";
+    break;
+    case 35:
+    echo "ez itt 35";
+    break;
+    case 24:
+    echo "ez itt 24";
+    break;
+
+    default :
+    echo "nem találtunk semmit";
+    break;
+
+}
+
+
+
+?>
+    
+</body>
+</html>
