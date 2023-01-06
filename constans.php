8af9d1cf48c4d2cdf6554bb6caeba4e2f82d93d7
MadarasiD <98647320+MadarasiD@users.noreply.github.com>
2023-01-06 12:30:24 +0100

    php constans

---
+++ constans.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Constans</title>
+</head>
+<body>
+
+<?php 
+
+$number = 10;
+
+$number = 1000;
+
+$number = "Dani";
+
+echo $number;
+echo "<br>";
+
+define("NAME", 1000);
+
+// NAME = 2000;
+
+echo NAME;
+
+?>
+    
+</body>
+</html>
